456b87d7d155a48328e4c2657f46137b140008f1
Hudson-Matias <hudson.um@gmail.com>
2024-08-26 19:50:41 -0300

    Utilizando o statelessWidget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("App Bar"),),
-      body: const Text("Meu App"),
+      appBar: AppBar(
+        title: const Text('Conta Bancária'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: const <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.account_balance_wallet),
+              title: Text('Conta Corrente'),
+              subtitle: Text('Saldo disponível: R\$ 1.000,00'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.savings),
+              title: Text('Conta Poupança'),
+              subtitle: Text('Saldo disponível: R\$ 5.000,00'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.credit_card),
+              title: Text('Cartão de Crédito'),
+              subtitle: Text('Fatura: R\$ 200,00'),
+            ),
+          ),
+        ],
+      ),
     );
   }
-
 }
